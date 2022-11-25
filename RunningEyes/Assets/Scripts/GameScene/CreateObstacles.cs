9d8b87dc0f9ef963ac6a782e7bb03be18542ea07
PorotOff <porotov31@gmail.com>
2022-11-25 19:09:02 +0700

    Делаю Гейм сцену
    Меню не закончено. Гейм сцена в разработке (делю движение дорог и препятствия).

---
+++ RunningEyes/Assets/Scripts/GameScene/CreateObstacles.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObstacles : MonoBehaviour
+{
+    private MoveMeneger _moveManager;
+
+    public void CreateObstaclesMethod()
+    {
+
+    }
+}
