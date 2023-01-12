8714e65714599457c1a65c0a41b04758e04973fd
PorotOff <119044006+PorotOff@users.noreply.github.com>
2023-01-13 06:51:27 +0700

    этой залупой я вообще не пользовался
    бубс 2

--- multe_game-Sega/RunningEyes/Assets/Scripts/GameScene/MoveAloneObject.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class MoveAloneObject : MonoBehaviour
-{
-    private void Update() {
-        gameObject.transform.Translate(Vector3.left * SpeedMoveSetting._speedMove * Time.deltaTime, Space.World);
-    }
-}
--- multe_game-Sega/RunningEyes/Assets/Scripts/ShopScene/SkinsChoice.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SkinsChoice : MonoBehaviour
-{
-    public List<GameObject> _playerPrefabs; 
-}
