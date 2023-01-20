9b87c314e60788d7d210a3e8cb9a424c8f4afbe9
MrMateriya <Gamearashow@gmail.com>
2023-01-20 18:09:08 +0700

    PersonControler

--- Platformer/Assets/scripts/MovmentPlayer.cs
+++ Platformer/Assets/scripts/MovmentPlayer.cs
@@ -6,16 +6,40 @@ public class MovmentPlayer : MonoBehaviour
 {
     [SerializeField] private float speed = 1;
     [SerializeField] private float ForceJump = 1;
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private LayerMask GroundLayer;
+    private float _Horizontal;
+    private bool IsFacingRight = true;
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    void Update()
+    {
+        _Horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, ForceJump);
+        }
+    }
     void FixedUpdate()
-    { 
-        transform.position += new Vector3(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0,0);
-        transform.position += new Vector3(0,Input.GetAxis("Vertical")*ForceJump*Time.deltaTime,0);
+    {
+        _rb.velocity = new Vector2(_Horizontal * speed * Time.deltaTime, _rb.velocity.y);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(GroundCheck.position,0.2f,GroundLayer);
+    }
+    void Flip()
+    {
+        if(IsFacingRight && _Horizontal < 0f || !IsFacingRight && _Horizontal>0f)
+        {
+            IsFacingRight = !IsFacingRight;
+            Vector3 flip = transform.localScale;
+            flip.x *= -1f;
+            transform.localScale = flip;
+        }
     }
 }
