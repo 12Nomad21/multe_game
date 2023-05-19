7db12a5c2f70f1b88e3a5ebf4c0f0f5d6ef1d7b2
PorotOff <porotov31@gmail.com>
2023-05-19 14:13:13 +0700

    пупа
    фыв

--- OtborochniyeFiles/Porotov/Assets/Scripts/Controlling/CreateTasks/OpenCreateTasksWindow.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class OpenCreateTasksWindow : MonoBehaviour
-{
-    public void OpenWindowOfCreateTasks(){
-        UserLogin.MainCamera.transform.position = new Vector3(0, (int)UserLogin.Windows.createTask * -10, UserLogin.MainCamera.transform.position.z);
-    }
-}
--- OtborochniyeFiles/Porotov/Assets/Scripts/SignOut.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SignOut : MonoBehaviour
-{
-    [SerializeField] private Camera mainCamera;
-    public void Exit(){
-        mainCamera.transform.position = new Vector3(0, 0, mainCamera.transform.position.z);
-    }
-}
